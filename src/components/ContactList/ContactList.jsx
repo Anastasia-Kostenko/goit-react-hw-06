dca6935635c5b10d0472db4c383e1fa18e50c635
Anastasia-Kostenko <139360670+Anastasia-Kostenko@users.noreply.github.com>
2024-04-03 19:45:47 +0300

    corection_redux_contactlist

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -7,14 +7,14 @@ import css from "./ContactList.module.css";
 
 export const ContactList = () => {
   const dataContact = useSelector(getContact);
-  const filtQuery = useSelector(getFilter);
+  const filterQuery = useSelector(getFilter);
 
-  const filtData = dataContact.filter((data) => {
-    return data.name.toLowerCase().includes(filtQuery.toLowerCase());
+  const filterData = dataContact.filter((data) => {
+    return data.name.toLowerCase().includes(filterQuery.toLowerCase());
   });
   return (
     <ul className={css.list}>
-      {filtData.map((data) => {
+      {filterData.map((data) => {
         return <Contact key={data.id} dataContact={data} />;
       })}
     </ul>
